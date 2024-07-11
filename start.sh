203677ed430119da0e9dcfd6c3f5dfb930a489af
Jakub Zilinek <zilinek.jakub@gmail.com>
2024-07-11 11:27:03 +0200

    added init script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-helm install argo argo/argo-cd --namespace=argocd --create-namespace --version 7.3.3 --values argocd-values.yaml
+helm install argo argo/argo-cd --namespace=argocd --create-namespace --version 7.3.3 --values argocd-values.yaml > /dev/null
 
-helm install root-sync-app root-sync-app --create-namespace
+helm install root-sync-app root-sync-app --create-namespace > /dev/null
+
+echo "Password is $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)"
 
